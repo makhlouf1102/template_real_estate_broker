8549a490716e151f300aca4b0e6b3e008cd9cdf9
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2024-06-04 09:15:55 -0400

    footer 1/2 responsive needed

--- template_1/src/app/common/Footer/Footer.js
+++ template_1/src/app/common/Footer/Footer.js
@@ -1,52 +1,102 @@
+import { FaLocationDot, FaEnvelope, FaPhone } from "react-icons/fa6";
+import { socialMedia } from '../socialMedia/commons';
+import Link from 'next/link';
+import { useTranslations } from "next-intl";
+
 const Footer = () => {
+  const space = "space-y-3"
+  const currentYear = new Date().getFullYear()
+  const tNav = useTranslations("Header.Navigation")
+  const tInfo = useTranslations("Header.Info")
+
   return (
-    <footer className="bg-white text-gray-700 py-10">
-      <div className="container mx-auto grid grid-cols-5 gap-6">
-        <div className="space-y-2">
-          <h4 className="text-lg font-bold">ABOUT ME</h4>
-          <ul className="space-y-1">
-            <li><a href="#about-me" className="hover:underline">About me</a></li>
-            <li><a href="#specialists" className="hover:underline">My specialists</a></li>
-            <li><a href="#testimonials" className="hover:underline">Testimonials</a></li>
-          </ul>
-        </div>
-        <div className="space-y-2">
-          <h4 className="text-lg font-bold">BUY</h4>
-          <ul className="space-y-1">
-            <li><a href="#real-estate-alert" className="hover:underline">Real estate alert</a></li>
-            <li><a href="#become-owner" className="hover:underline">Become an owner</a></li>
-          </ul>
-          <h4 className="text-lg font-bold mt-4">SELL</h4>
-          <ul className="space-y-1">
-            <li><a href="#property-assessment" className="hover:underline">Property assessment</a></li>
-            <li><a href="#selling-property" className="hover:underline">Selling your property</a></li>
-          </ul>
-        </div>
-        <div className="space-y-2">
-          <h4 className="text-lg font-bold">PROPERTIES</h4>
-        </div>
-        <div className="space-y-2">
-          <h4 className="text-lg font-bold">JOURNAL</h4>
-        </div>
-        <div className="space-y-2">
-          <h4 className="text-lg font-bold">CONTACT ME</h4>
-          <address className="not-italic space-y-1">
-            <p>1290 rue Bernard Ouest<br />
-              Office 100<br />
-              Montréal, QC, H2V 1V9</p>
-            <a href="mailto:info@mblaise.com" className="block hover:underline">info@mblaise.com</a>
-            <a href="tel:+15142712131" className="block hover:underline">514 271-2131 (off.)</a>
-            <a href="tel:+15149933557" className="block hover:underline">514 993-3357 (cell)</a>
-          </address>
-        </div>
-        <div className="col-span-5 flex justify-center mt-6">
-          <div className="text-center">
-            <img src="remax-logo.png" alt="RE/MAX du Cartier" className="mx-auto mb-2" />
-            <p className="text-gray-700">Agence immobilière</p>
+    <>
+
+      <footer className="w-full flex-col justify-center">
+        <div className="hidden lg:flex px-4 w-full justify-center">
+          <div className="w-3/5 pt-16 pb-20 ">
+            <div className="container grid grid-cols-4 gap-x-10">
+              <div className={`${space}`}>
+                <h4 className="text-lg font-bold">{tNav("about")}</h4>
+                <ul className="space-y-2 text-sm">
+                  <li><Link href="#about-me" className="hover:opacity-70">{tNav("about")}</Link></li>
+                  <li><Link href="#specialists" className="hover:opacity-70">{tNav("specialities")}</Link></li>
+                  <li><Link href="#testimonials" className="hover:opacity-70">{tNav("testimonials")}</Link></li>
+                </ul>
+                <div>
+                  <Link href="#blog" className="text-lg font-bold hover:opacity-70">{tNav("blog")}</Link>
+                </div>
+                <div>
+                  <Link href="#properties" className="text-lg font-bold hover:opacity-70">{tNav("properties")}</Link>
+                </div>
+              </div>
+              <div className={`${space}`}>
+                <h4 className="text-lg font-bold">{tNav("buy")}</h4>
+                <ul className="space-y-1">
+                  <li><Link href="#real-estate-alert" className="hover:opacity-70">{tNav("real-estate-alert")}</Link></li>
+                  <li><Link href="#become-owner" className="hover:opacity-70">{tNav("buy-a-property")}</Link></li>
+                </ul>
+                <h4 className="text-lg font-bold mt-4">{tNav("sell")}</h4>
+                <ul className="space-y-1">
+                  <li><Link href="#property-assessment" className="hover:opacity-70">{tNav("property-assessment")}</Link></li>
+                  <li><Link href="#selling-property" className="hover:opacity-70">{tNav("sell-your-property")}</Link></li>
+                </ul>
+              </div>
+              <div className={`${space}`}>
+                <Link href="#contact-me" className="text-lg font-bold">{tNav("contact")}</Link>
+                <address className="not-italic flex flex-col gap-y-3">
+                  <span className="flex gap-2">
+                    <FaLocationDot />
+                    <p>
+                      XXXX Web Street<br />
+                      Office 999<br />
+                      City, XX, XXX XXX
+                    </p>
+                  </span>
+                </address>
+                <span className="flex gap-2">
+                  <FaPhone />
+                  <Link href="tel:+19999999999" className="block hover:opacity-70 font-bold">999 999-9999 (off.)</Link>
+                </span>
+                <span className="flex gap-2">
+                  <FaPhone />
+                  <Link href="tel:+19999999999" className="block hover:opacity-70 font-bold">999 999-9999 (cell)</Link>
+                </span>
+                <span className="flex gap-2">
+                  <FaEnvelope />
+                  <Link href="mailto:email@email.com" className="block hover:opacity-70 font-bold">email@email.com</Link>
+                </span>
+
+              </div>
+              <div className={`${space}`}>
+                <div className="text-center">
+                  <img src="remax-logo.png" alt="RE/MAX du Cartier" className="mx-auto mb-2" />
+                  <p className="text-gray-700">Agence immobilière</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
+        <div className="border-t border-gray-200 w-full flex justify-center">
+          <div className="w-3/5 flex justify-between py-4">
+            <span className="w-3/4 space-x-4 grid grid-cols-3 justify-between">
+              <p className="font-bold text-sm">© {currentYear} {tInfo("copyright")}</p>
+              <Link href="#terms-of-use" className="hover:opacity-70 font-bold text-sm"> Web Conception | 1102 Solution</Link>
+              <Link href="#privacy-policy" className="hover:opacity-70 font-bold text-sm">{tInfo("privacy-policy")}</Link>
+            </span>
+            <span className="flex gap-4">
+              {socialMedia.map((item) => (
+                <span key={item.title} className='hover:opacity-70'>
+                  <Link href={item.link} target='_blank'>
+                    {item.icon}
+                  </Link>
+                </span>
+              ))}
+            </span>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
 
