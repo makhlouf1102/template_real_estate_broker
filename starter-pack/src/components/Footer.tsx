7388dda0b5953f8a9c72f25bc192f4bb5f92487d
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2024-06-10 19:39:42 -0400

    Footer done

--- starter-pack/src/components/Footer.tsx
+++ starter-pack/src/components/Footer.tsx
@@ -1,9 +1,9 @@
 import { AGENT_INFO } from '@/constantes/agent.const';
 import { useTranslations } from 'next-intl';
-import Image from 'next/image';
 import Link from 'next/link';
-import { Button } from "@nextui-org/react";
-import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+import { unstable_setRequestLocale } from 'next-intl/server';
+import { FaFacebook, FaXTwitter, FaInstagram, FaLinkedin } from "react-icons/fa6";
+
 
 
 export default function Footer({ params: { locale } }: { params: { locale: string } }) {
@@ -12,22 +12,51 @@ export default function Footer({ params: { locale } }: { params: { locale: strin
     unstable_setRequestLocale(locale);
 
     const t = useTranslations('Footer');
+
+    const section_style = "flex items-center gap-2"
     return (
         <>
             <footer className="bg-white border-y border-gray-100 py-4">
-                <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-                    <div className="contact-info text-center md:text-left">
-                        <p>Adresse: Rue de l'Immobilier, Ville, Code Postal</p>
-                        <p>Téléphone: +33 1 23 45 67 89</p>
-                        <p>Email: contact@agentimmobilier.com</p>
+                <div className="container mx-auto flex flex-col justify-between items-center gap-4">
+                    <div className="flex flex-col items-center">
+
+                        <div className={`${section_style}`}>
+                            <span className="font-bold">{t("office")} : </span>
+                            <Link href={`https://www.google.com/maps/search/?api=1&query=${AGENT_INFO.address}`} target="_blank" className="hover:opacity-50">
+                                {AGENT_INFO.address}
+                            </Link>
+
+                        </div>
+                        <div className={`${section_style}`}>
+                            <span className="font-bold">{t("phone")} : </span>
+                            <Link href={`tel:${AGENT_INFO.phone}`} className="hover:opacity-50">
+                                {AGENT_INFO.phone}
+                            </Link>
+                        </div>
+
+                        <div className={`${section_style}`}>
+                            <span className="font-bold">{t("email")} :</span>
+                            <Link href={`mailto:${AGENT_INFO.email}`} className="hover:opacity-50">
+                                {AGENT_INFO.email}
+                            </Link>
+                        </div>
                     </div>
-                    <div className="social-media flex space-x-4 mt-4 md:mt-0">
-                        <Link href="#" className="text-text-50 hover:underline">Facebook</Link>
-                        <Link href="#" className="text-text-50 hover:underline">Twitter</Link>
-                        <Link href="#" className="text-text-50 hover:underline">LinkedIn</Link>
+                    <div className="flex gap-4 mt-4 md:mt-0 text-text-50">
+                        <Link href="#" className="hover:opacity-50">
+                            <FaFacebook />
+                        </Link>
+                        <Link href="#" className="hover:opacity-50">
+                            <FaXTwitter />
+                        </Link>
+                        <Link href="#" className="hover:opacity-50">
+                            <FaInstagram />
+                        </Link>
+                        <Link href="#" className="hover:opacity-50">
+                            <FaLinkedin />
+                        </Link>
                     </div>
                     <div className="legal text-center md:text-right mt-4 md:mt-0">
-                        <p><Link href="#" className="text-accent-500 hover:underline">Mentions légales</Link> | <Link href="#" className="text-accent-500 hover:underline">Politique de confidentialité</Link></p>
+                        <p><Link href="#" className="text-accent-500 hover:opacity-50">{t("legal")}</Link> | <Link href="#" className="text-accent-500 hover:opacity-50">{t("privacy")}</Link></p>
                     </div>
                 </div>
             </footer>
