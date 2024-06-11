f383349c3394f935bb76249fb4347800b1458857
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2024-06-11 14:43:25 -0400

    About section added

--- starter-pack/src/app/[locale]/components/About.tsx
+++ starter-pack/src/app/[locale]/components/About.tsx
@@ -6,10 +6,13 @@ export default function About() {
     const t = useTranslations("About");
 
     return (
-        <section id="about">
-            <div className="about-content">
+        <section id="about"> 
+            <div className="container mx-auto flex flex-col md:flex-row md:justify-between gap-10 py-20 px-10">
                 <Image src="/image.png" alt="Photo de l'agent immobilier" width={500} height={500} />
-                <p>{t("description")}</p>
+                <div className="about-text flex flex-col gap-2">
+                    <h2 className="text-3xl font-bold">{t("title")}</h2>
+                    <p>{t("description")}</p>
+                </div>
             </div>
         </section>
     )
