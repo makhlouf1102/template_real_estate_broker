9a12cb34e1e42ca15ea25bdc2fa5895a89ed93ee
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2024-06-16 23:05:48 -0400

    removing all unused files

--- template_1/src/app/[locale]/components/testimonials.js
+++
@@ -1,6 +0,0 @@
-import { useTranslations } from "next-intl";
-
-export default function Testimonials() {
-  return <div>Testimonials</div>;
-}
-
--- template_1/src/app/common/Header/NavigationConsts.js
+++
@@ -1,8 +0,0 @@
-export const navigationLinks = [
-  {
-    title: '(999) 999 99999',
-    link: 'tel:(999) 999 99999',
-    // icon: HiPhoneArrowUpRight,
-    icon: "null",
-  },
-];
