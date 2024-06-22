3fa52476fe0a2efd920eff7fd4ebad82235404ff
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2024-06-22 18:08:09 -0400

    changing the footer

--- starter-pack/src/components/Footer.tsx
+++ starter-pack/src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import {LocaleProps} from '@props/Locale.props';
+import { LocaleProps } from '@props/Locale.props';
 import Link from 'next/link';
-import {useTranslations} from 'next-intl';
-import {unstable_setRequestLocale} from 'next-intl/server';
-import {FaFacebook, FaXTwitter, FaInstagram, FaLinkedin} from 'react-icons/fa6';
-import {AGENT_INFO} from '@/constantes/agent.const';
+import { useTranslations } from 'next-intl';
+import { unstable_setRequestLocale } from 'next-intl/server';
+import { FaFacebook, FaXTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa6';
+import { AGENT_INFO } from '@/constantes/agent.const';
 
-export default function Footer({locale}: LocaleProps) {
+export default function Footer({ locale }: LocaleProps) {
   // enable to render static
   unstable_setRequestLocale(locale);
 
@@ -17,6 +17,22 @@ export default function Footer({locale}: LocaleProps) {
       <footer className="border-y border-gray-100 bg-white py-4">
         <div className="container mx-auto flex flex-col items-center justify-between gap-4">
           <div className="flex flex-col items-center">
+            <div className="mt-4 flex gap-4 text-text-50 md:mt-0 items-center my-4">
+              <hr className="w-96 h-0.5 mx-auto bg-gray-300 border-0 rounded" />
+              <Link className="hover:opacity-50" href="#">
+                <FaFacebook />
+              </Link>
+              <Link className="hover:opacity-50" href="#">
+                <FaXTwitter />
+              </Link>
+              <Link className="hover:opacity-50" href="#">
+                <FaInstagram />
+              </Link>
+              <Link className="hover:opacity-50" href="#">
+                <FaLinkedin />
+              </Link>
+              <hr className="w-96 h-0.5 mx-auto bg-gray-300 border-0 rounded" />
+            </div>
             <div className={`${section_style}`}>
               <span className="font-bold">{t('office')} : </span>
               <Link
@@ -47,20 +63,7 @@ export default function Footer({locale}: LocaleProps) {
               </Link>
             </div>
           </div>
-          <div className="mt-4 flex gap-4 text-text-50 md:mt-0">
-            <Link className="hover:opacity-50" href="#">
-              <FaFacebook />
-            </Link>
-            <Link className="hover:opacity-50" href="#">
-              <FaXTwitter />
-            </Link>
-            <Link className="hover:opacity-50" href="#">
-              <FaInstagram />
-            </Link>
-            <Link className="hover:opacity-50" href="#">
-              <FaLinkedin />
-            </Link>
-          </div>
+
           <div className="legal mt-4 text-center md:mt-0 md:text-right">
             <p>
               <Link className="text-accent-500 hover:opacity-50" href="#">
