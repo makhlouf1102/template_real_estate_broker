8d2e6bd36f1dfc540d5fdab6a55ef2737f0b0593
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2024-07-03 09:51:56 -0400

    Adding callendly

--- starter-pack/src/app/[locale]/components/BookCallBanner.tsx
+++ starter-pack/src/app/[locale]/components/BookCallBanner.tsx
@@ -1,22 +1,23 @@
-import { Button } from '@nextui-org/react';
+'use client';
 import { useTranslations } from 'next-intl';
 import React from 'react';
+import { InlineWidget } from 'react-calendly';
 
 function BookCallBanner() {
   const t = useTranslations('BookCallBanner');
 
   return (
-    <section className="bg-background-50 text-center">
-      <div className="container mx-auto px-5 py-5 sm:px-10 sm:py-6 md:px-20 md:py-8 lg:px-32 lg:py-10 flex flex-col sm:flex-row items-center justify-around gap-5 sm:gap-10">
-        <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-white mb-4 sm:mb-0">
+    <section className="bg-background-50 p-6 sm:p-8 md:p-10 lg:p-12">
+      <div className="container mx-auto text-center">
+        <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-white mb-4">
           {t('title')}
         </h3>
-        <Button
-          className="bg-accent-500 font-bold text-white hover:bg-accent-600"
-          color="secondary"
-        >
-          {t('button')}
-        </Button>
+        <div>
+          <InlineWidget 
+            url="https://calendly.com/mak-hennine/30min" 
+            styles={{ height: '900px', minHeight: '500px', borderRadius: '8px' }} 
+          />
+        </div>
       </div>
     </section>
   );
