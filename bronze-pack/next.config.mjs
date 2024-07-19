1c04c905a2146b6ebd1c8646e5af7aaa504ef87c
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2024-07-19 11:07:55 -0400

    project works, now have to add more pages

--- bronze-pack/next.config.mjs
+++ bronze-pack/next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withNextIntl from 'next-intl/plugin';
 
-export default nextConfig;
+const nextConfig = {
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        // ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
+
+export default withNextIntl()(nextConfig);
